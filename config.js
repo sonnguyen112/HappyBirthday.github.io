d143e893a4c7bf5eb9c5e5fb853168e448de3f2e
SonNguyenMaico112 <truongson.maicogroup@gmail.com>
2021-12-01 09:08:58 +0700

    áads

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Duy à!',
+    introDesc: `Hãy chọn một trong 2 điều ước và ước đi`,
+    btnIntro: 'vô chọn điều ước',
+    title: 'Hãy suy nghĩ thật kĩ và chọn nhá 😙',
+    desc: 'Nhớ là hãy chắc chắn mới chọn nhá',
+    btnYes: 'Sinh nhật thật vui vẻ',
+    btnNo: 'Đẹp trai ngang David Beckham :3',
+    question:'Hãy nói ra cảm nhận của Duy về ngày sinh nhật với thế giới',
+    btnReply: 'Gửi cho thế giới',
+    reply: 'Sinh nhật hôm nay thật hạnh phúc',
+    mess: 'Thế giới biết mà 🥰. Sinh nhật vui vẻ nhá 😘😘',
+    messDesc: 'Hôm nay quẩy hết mìnhhhhhhhh',
+    btnAccept: 'Okiiiii lun <3!',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
